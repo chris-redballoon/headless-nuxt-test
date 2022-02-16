897ed60e1d26143edc976e402313f939153edd95
Chris Cunningham <chris@redballoon.io>
2022-02-16 09:54:18 +0000

    add yarn packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 let dynamicRoutes = () => {
 return axios
-  .get("http://headless-test-nuxt.local/wp-json/wp/v2/posts?page=1&per_page=20")
+  .get("https://headless-test-nuxt.local/wp-json/wp/v2/posts?page=1&per_page=20")
   .then(res => {
     return res.data.map(post => `/blog/${post.slug}`)
   })
