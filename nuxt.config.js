963d7292731abca84f85e07c92e949e11298ebd5
Chris Cunningham <chris@redballoon.io>
2022-02-16 10:16:17 +0000

    change path to dynamically grab posts

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ let dynamicRoutes = () => {
 return axios
   .get("https://spd.balloonhost.co.uk/wp-json/wp/v2/posts?page=1&per_page=20")
   .then(res => {
-    return res.data.map(post => `/blog/${post.slug}`)
+    return res.data.map(post => `${post.slug}`)
   })
 }
 
